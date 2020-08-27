555988ad69608e3ef0d5bfaa941afa03a8847c7c
Dominik Szarkiel <d.szarkiel@gmail.com>
2020-08-27 15:54:53 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,7 +6,6 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index(user_input)
+def input_to_index("1" - "9")
   "user_input".to_i
-  ("user_input" - "1")
 end
